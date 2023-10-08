fdba5bbd13f5146bace8ed9d8e26557968a4f37a
KAKXER <89189991+KAKXER@users.noreply.github.com>
2023-10-08 17:32:10 +0330

    Create config.lua

---
+++ server/config.lua
@@ -0,0 +1,12 @@
+-- ! ███████╗██╗██╗   ██╗███████╗███████╗████████╗ █████╗ ██████╗ 
+-- ! ██╔════╝██║██║   ██║██╔════╝██╔════╝╚══██╔══╝██╔══██╗██╔══██╗
+-- ! █████╗  ██║██║   ██║█████╗  ███████╗   ██║   ███████║██████╔╝
+-- ! ██╔══╝  ██║╚██╗ ██╔╝██╔══╝  ╚════██║   ██║   ██╔══██║██╔══██╗
+-- ! ██║     ██║ ╚████╔╝ ███████╗███████║   ██║   ██║  ██║██║  ██║
+-- ! ╚═╝     ╚═╝  ╚═══╝  ╚══════╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝
+-- ! 		   Copyright ® 2023 Lorem All rights FiveStar
+-- ! 		    5star.tebex.io | Discord.ggHdEzqEJBdh
+Config = {}
+
+-- * If an update is released for the script, it will not allow the script to start and will cause the server to shut down
+Config.CheckVersion = true
